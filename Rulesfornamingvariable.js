032672bee666cac3c349f4c348b64ce23a4aff49
Abhishek Gupta <abhishekgupta0301@gmail.com>
2023-12-01 00:45:44 +0530

    rules for naming variable

---
+++ Rulesfornamingvariable.js
@@ -0,0 +1,9 @@
+//  rules for naming variable
+// you can not start with numbers
+//  var 1value=10; invalid
+var value1=10;
+console.log(value1 ** 2);
+
+//  you can use only underscore _ or $ symbol
+// you can not use spaces
+// use camelcase
---
+++ variable.js
@@ -0,0 +1,12 @@
+// variable declare
+
+var Name ="anit";
+
+//  use a variable
+
+console.log(Name);
+
+// change value
+ 
+Name = "arnav";
+console.log(Name);
