c7aeef7fc084d8031256e9f9832d88db857744ba
Abhishek Gupta <abhishekgupta0301@gmail.com>
2023-12-06 01:49:56 +0530

    i am using vs code terminal for output because i install node js in my pc so i dont need to inspect the any site for console

--- String.js
+++ String.js
@@ -1,4 +1,24 @@
 //  string indexing 
-let firstName ="harshit";
+// let firstName ="harshit";
 //  length of string
- console.log(firstName.length);
+//  console.log(firstName.length);
+//   how to find the last index address of an array
+// console.log(firstName[firstName.length-1]);
+//  methods of string
+//  trim()
+//  toUppercase()
+// toLowercase()
+// slice
+
+
+//  trim()
+
+let firstName="  harshit   ";
+ 
+console.log(firstName.length);
+//  we can do trim string in two ways 
+//  1 is this 
+ let newString= firstName.trim();
+ console.log(newString)
+ console.log(newString.length);
+
