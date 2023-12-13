d06d989084cf45cf2aaf3fb99bc7870fc69141da
Abhishek Gupta <abhishekgupta0301@gmail.com>
2023-12-13 23:35:58 +0530

    working

---
+++ typefoperator.js
@@ -0,0 +1,10 @@
+// type of operator 
+ let age =22;
+ let firstName="Abhishek Gupta"
+ console.log(typeof age);
+ console.log(typeof firstName);
+
+
+//   convert number to string
+
+console.log(typeof (age + ""));
