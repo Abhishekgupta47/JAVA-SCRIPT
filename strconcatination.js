09d6e03f975aef21601175b24047d87a7ae95608
Abhishek Gupta <abhishekgupta0301@gmail.com>
2023-12-14 00:27:37 +0530

    templete string

--- strconcatination.js
+++ strconcatination.js
@@ -1,5 +1,14 @@
-let string1 = "abhi";
-let string2 ="gupta";
+// let string1 = "abhi";
+// let string2 ="gupta";
 
-let fullName= string1+" "+string2;
-console.log(fullName);
+// let fullName= string1+" "+string2;
+// console.log(fullName);
+
+//  templete string 
+
+let age =21;
+ let firstName = "abhishek";
+  
+//  let aboutMe = "My name is "+firstName +" my age is "+ age;
+ let aboutMe=`my name is ${firstName}  my age is ${age} `;
+ console.log(aboutMe);
