46ceb16faf64f03fbde6a957afb6d78d266054b0
Abhishek Gupta <abhishekgupta0301@gmail.com>
2023-12-15 23:32:40 +0530

    i think operators and / or we can do our self so i dont think so we need to do it in these repository

---
+++ ternary.js
@@ -0,0 +1,20 @@
+// // ternary operator
+
+//  we can do this in this methos but it is too lenghty
+
+// let age =4;
+//  let drink;
+//  if(age>=5){
+//   drink ="coffee";
+
+//  }
+//  else{
+//   drink ="milk";
+//  }
+//  console.log(drink);
+
+// by ternary operator / conditional operator
+ 
+let age = 8;
+let drink = age >= 5 ? "coffee" : "milk"
+console.log(drink);
