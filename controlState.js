2601eeb4b95008b721aa193df540407f23cf9a6b
Abhishek Gupta <abhishekgupta0301@gmail.com>
2023-12-15 23:21:30 +0530

    ...

--- controlState.js
+++ controlState.js
@@ -1,5 +1,13 @@
 //  if else condition
- let age =19;
+ let age= 19;
+ 
  if(age>18){
   console.log("you can play ddlc");
  }
+ let num =14;
+  if(num%2==0){
+    console.log("even");
+
+  }else{
+    console.log("odd");
+  }
