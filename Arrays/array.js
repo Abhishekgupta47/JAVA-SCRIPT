a7395eeaca74c465090112bbeee57ae91f18a0ae
Abhishek Gupta <abhishekgupta0301@gmail.com>
2023-12-16 15:40:10 +0530

    definition and declaration

--- Arrays/array.js
+++ Arrays/array.js
@@ -4,4 +4,9 @@
 // "ordered collection of items";
 
 let fruits=["apple","banana","grapes","orange"];
-console.log(fruits);
+ let numbers=[1,2,3,4];
+ console.log(fruits);
+ console.log(numbers);
+//   we can add more tghan data type value in js arra
+let mixed =[1,2,2.4,"siyaRam",null,undefined];
+console.log(mixed);
