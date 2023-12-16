f9bc5ef3e62b6de2f84649c129130191f9594578
Abhishek Gupta <abhishekgupta0301@gmail.com>
2023-12-16 15:47:54 +0530

    how to check that we are functioning with array ?

--- Arrays/array.js
+++ Arrays/array.js
@@ -10,3 +10,7 @@ let fruits=["apple","banana","grapes","orange"];
 //   we can add more tghan data type value in js arra
 let mixed =[1,2,2.4,"siyaRam",null,undefined];
 console.log(mixed);
+console.log(typeof mixed);
+let obj={};
+console.log(Array.isArray(fruits));
+console.log(Array.isArray(obj));
