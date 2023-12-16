92f5d5aeefa8ef025fb554b29a760a8e1c716920
Abhishek Gupta <abhishekgupta0301@gmail.com>
2023-12-16 16:14:06 +0530

    unsift done

--- Arrays/PushPop.js
+++ Arrays/PushPop.js
@@ -12,5 +12,11 @@ console.log(fruits);
 
 
 //  pop
-console.log(fruits.pop());
-console.log(fruits);
+// console.log(fruits.pop());
+// console.log(fruits);
+
+
+//  shift / unsift
+//  fruits.unshift("mango");
+//  fruits.unshift("myfruit");
+//  console.log(fruits);
